259d574ad175d97691bafcfc13c4e85de972c936
BrianZhao10 <brianzhaoptimal@gmail.com>
2026-09-19 13:36:52 -0400

    Icons' Calling

--- app/modal.tsx
+++ app/modal.tsx
@@ -17,7 +17,7 @@ export default function ModalScreen() {
           <View style={styles.brandTop}>
             <View style={styles.brandMark}>
               <View style={styles.brandMarkInner}>
-                <Text style={styles.brandMarkText}>M</Text>
+                <Text style={styles.brandMarkText}>◖⬮◗</Text>
               </View>
             </View>
 
