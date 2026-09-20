a8cde416983eee12fbce00e2c1fe91d2a613992a
BrianZhao10 <brianzhaoptimal@gmail.com>
2026-09-19 22:51:59 -0400

    Arrete Error Test Arrest

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -8,7 +8,6 @@ import {
 } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Text, View } from '@/components/Themed';
-import { createClient } from '@supabase/supabase-js';
 
 type TranscriptSegment = {
   text: string;
